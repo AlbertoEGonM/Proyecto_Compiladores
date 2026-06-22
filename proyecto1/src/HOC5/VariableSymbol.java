d80adf8dfe45ead7a275802b9d3190fba4d5a3b0
vanevan12 <hkvmq2@gmail.com>
2026-06-21 22:08:29 -0600

    Add files via upload

---
+++ proyecto1/src/HOC5/VariableSymbol.java
@@ -0,0 +1,36 @@
+package HOC3;
+
+public class VariableSymbol extends SymbolHoc {
+    private double value;
+    private final boolean isConstant; // Para proteger PI o E de ser sobreescritas
+
+    // Constructor para variables ordinarias (se inicializan en 0.0)
+    public VariableSymbol(String name, int tokenType) {
+        super(name, tokenType);
+        this.value = 0.0;
+        this.isConstant = false;
+    }
+
+    // Constructor para constantes matemáticas
+    public VariableSymbol(String name, int tokenType, double value) {
+        super(name, tokenType);
+        this.value = value;
+        this.isConstant = true;
+    }
+
+    public boolean isConstant() {
+        return this.isConstant; // Retorna el atributo booleano de la clase
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        if (isConstant) {
+            System.err.println("Error: No se puede reasignar un valor a la constante " + getName());
+            return; 
+        }
+        this.value = value;
+    }
+}
